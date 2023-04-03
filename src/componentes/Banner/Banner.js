918c204321f69a187d2007b704f61ed4b70fd257
Sérgio Lima <sergiolimafg3@gmail.com>
2023-04-03 12:30:25 -0300

    projeto organo criano com node js insserido img criado o componente header e seu respectivo css

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner () {
+  // JSX
+  return (
+    <header className='banner'>
+      <img src="/imagens/Banner.png" alt="imagem do site orni"/>
+    </header>
+  )
+}
+
+export default Banner
