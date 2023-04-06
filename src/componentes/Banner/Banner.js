b553c2ce40a97e4b613bd266161d5bcb06cd1a6a
Sérgio Lima <sergiolimafg3@gmail.com>
2023-04-06 13:13:05 -0300

    inserindo  informação com usso de fragment

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,11 +1,15 @@
+import { Fragment } from 'react'
 import './Banner.css'
 
 function Banner () {
-  // JSX
+  // JSX uso de fragmente (<> </>) AND <fragmente></fragmente>
   return (
+    <Fragment> 
     <header className='banner'>
       <img src="/imagens/Banner.png" alt="imagem do site orni"/>
+      <h1>Sistema de Cadastro de passageiros e Rotas de viagem</h1>
     </header>
+    </Fragment>
   )
 }
 
