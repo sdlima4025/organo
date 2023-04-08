1461d60f0409eabf7abe05907fdc87a6c2823b65
Sérgio Lima <sergiolimafg3@gmail.com>
2023-04-08 12:21:43 -0300

    corrigido bug banner

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -6,7 +6,7 @@ function Banner () {
   return (
     <Fragment> 
     <header className='banner'>
-      <img src="/imagens/Banner.png" alt="imagem do site orni"/>
+      <img src="/imagens/banner.png" alt="imagem do site organo"/>
       <h1>Sistema de Cadastro de passageiros e Rotas de viagem</h1>
     </header>
     </Fragment>
